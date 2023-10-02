32afd8022de8c2f24b264dc644e24a8e8cacdda9
Michael <michael.scharf@consetto.com>
2023-10-02 22:36:06 +0200

    charts added, colors changed, switch added

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,25 +1,109 @@
-import React from "react";
-import { Card, Text, CardHeader } from "@ui5/webcomponents-react";
+import React, { useState } from "react";
+import { Card, CardHeader, Text, Icon } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import lineChartIcon from '@ui5/webcomponents-icons/dist/line-chart.js';
+import barChartIcon from '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
+import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme'
+import '@ui5/webcomponents/dist/Assets';
+import '@ui5/webcomponents-fiori/dist/Assets';
 
+
+
+const dataset = [
+    {
+        month: "January",
+        data: 65
+    },
+    {
+        month: "February",
+        data: 59
+    },
+    {
+        month: "March",
+        data: 80
+    },
+    {
+        month: "April",
+        data: 81
+    },
+    {
+        month: "May",
+        data: 56
+    },
+    {
+        month: "June",
+        data: 55
+    },
+    {
+        month: "July",
+        data: 40
+    }
+];
+setTheme('sap_horizon_dark')
 export function MyApp() {
 
-   
+    const [toggleCharts, setToggleCharts] = useState("lineChart");
+    const [loading, setLoading] = useState(false);
+
     const handleHeaderClick = () => {
-          alert("Header clicked");
+        if (toggleCharts === "lineChart") {
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+            setToggleCharts("barChart");
+          }, 2000);
+        } else {
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+            setToggleCharts("lineChart");
+          }, 2000);
+        }
     };
-      
-  return (
-    <div>
-      <Card header={<CardHeader titleText="Card" 
-      interactive 
-      onClick={handleHeaderClick} 
-      />} 
-      style={{ width: "300px" }}>
-        <Text style={spacing.sapUiContentPadding}>
-          This is the content area of the Card
-        </Text>
-      </Card>
-    </div>
-  );
+
+    const contentTitle = toggleCharts === 'lineChart' ? 'Line Chart' : 'Bar Chart';
+    const switchToChart = toggleCharts === 'lineChart' ? 'Bar Chart' : 'Line Chart';
+
+
+    return (
+        <div>
+            <Card
+                header={
+                    <CardHeader
+                        titleText="Stock Prices"
+                        subtitleText={`Click here to switch to ${switchToChart}`}
+                        interactive
+                        onClick={handleHeaderClick}
+                        avatar={
+                            <Icon
+                                name={
+                                    toggleCharts === "lineChart" ? lineChartIcon : barChartIcon
+                                }
+                            />
+                        }
+                    />
+                }
+                style={{ width: "80%"}}
+            >
+                <Text style={spacing.sapUiContentPadding}>{contentTitle}</Text>
+                {toggleCharts === "lineChart" ? (
+                    <LineChart
+                        dimensions={[{ accessor: "month" }]}
+                        measures={[{ accessor: "data", label: "Stock Price" }]}
+                        dataset={dataset}
+                        loading={loading}
+                    />
+                ) : (
+                    <BarChart
+                        dimensions={[{ accessor: "month" }]}
+                        measures={[{ accessor: "data", label: "Stock Price" }]}
+                        dataset={dataset}
+                        loading={loading}
+                    />
+                )}
+            </Card>
+
+        </div>
+    );
 }
